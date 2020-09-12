d3966e66bb5f8d57225ee8d146f5d4fb8f51f176
barbfromhr <70112119+barbfromhr@users.noreply.github.com>
2020-09-12 18:14:29 -0400

    2nd try

---
+++ practicep5/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(600, 600);
+   background(255,0,0);}
+
+function draw() {
+//  background(255,0,0);
+  fill(255,0,0);
+  stroke(0);
+     rect(width/2, height/2, 500,300);
+  //background(255); //leaves no past loops, because background is redone
+  fill(random(80),random(80),random(85));
+ // fill(255);
+  // rect(random(600),random(400),random(100),random(100)); //random rectangle generator
+   ellipse(mouseX,mouseY,50,10); // random generator following mouse
+
+}
